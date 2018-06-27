e6b6d2f2dbf508dcd06a11cca4f907701ccdb1f2
Eric Liu <ericliu404@gmail.com>
2018-06-27 09:53:07 -0400

    Fix: codestyle and cancel button timing issue

--- source/js/plugins/pixel-wrapper.test.js
+++ source/js/plugins/pixel-wrapper.test.js
@@ -27,30 +27,28 @@ afterAll(() => {
 });
 
 describe('Check Proper Plugin Creation', () => {
-    var pluginIcon;
-
     test('page title matches', async () => {
         let title = await browser.getTitle();
         expect(title).toBe('Pixel.js');
     });
 
+    var pluginIcon;
     test('plugin icon exists', async () => {
         pluginIcon = await browser.findElement(By.id('diva-1-pixel-icon-glyph'));
         expect(pluginIcon).toBeDefined();
     });
-
     test('icon clicked creates tutorial', async () => {
         // activate plugin
         const actions = browser.actions();
         await actions.click(pluginIcon).perform();
 
         let tutorialDiv = await browser.findElement(By.id('tutorial-div'));
-        expect(tutorialDiv).toBeDefined();
+        expect(await tutorialDiv.isDisplayed()).toBeTruthy();
 
         // close tutorial and scroll down
+        await browser.sleep(500);
         let tutorialFooter = await browser.findElement(By.id('modal-footer'));
         await actions.click(tutorialFooter).perform();
-        await actions.keyDown(40).perform();
     });
 });
 
@@ -80,13 +78,13 @@ describe('Check Functionality', () => {
         await actions.click(exportPNGButton).perform();
 
         // links should now be visible
-        let links = await browser.findElement(By.id('png-links-div'));
-        expect(links).toBeDefined();
+        let links = await browser.wait(until.elementLocated(By.id('png-links-div')), 1000);
+        expect(await links.isEnabled()).toBeTruthy();
     });
 
     // next three tests are linked and use the submitButton variable
     var submitButton;
-    test('submit to rodan without selection regions creates alert', async() => {
+    test('submit to rodan without selection regions creates alert', async () => {
         // click on button
         submitButton = await browser.findElement(By.id('rodan-export-button'));
         const actions = browser.actions();
@@ -97,7 +95,7 @@ describe('Check Functionality', () => {
         expect(await alert.getText()).toBe("You haven't created any select regions!");
         await alert.accept();
     });
-    test('drawing select region then submit to rodan creates progress bar', async() => {
+    test('drawing select region then submit to rodan creates progress bar', async () => {
         let canvas = await browser.findElement(By.id('layer--1-canvas'));
         const actions = browser.actions();
         // select rectangle tool and draw one
@@ -109,12 +107,25 @@ describe('Check Functionality', () => {
         let progressBar = await browser.findElement(By.id('pbar-inner-div'));
         expect(await progressBar.isDisplayed()).toBeTruthy();
     });
-    test('cancel progress bar works', async() => {
+    test('cancel progress bar works', async () => {
+        await browser.sleep(1000);
+
         let cancelButton = await browser.findElement(By.id('cancel-export-div'));
         const actions = browser.actions();
         await actions.click(cancelButton).perform();
 
+        await browser.sleep(1000);
         expect(await browser.findElements(By.id('pixel-export-div'))).toEqual([]); // empty list since not found
+    });
+
+    test('layer 1 activation toggle works properly', async () => {
+        let layer1Toggle = await browser.findElement(By.id('layer-1-activation'));
+        expect(await layer1Toggle.getAttribute('class')).toBe('layer-activated');
+
+        const actions = browser.actions();
+        await actions.click(layer1Toggle).perform();
+
+        expect(await layer1Toggle.getAttribute('class')).toBe('layer-deactivated');
     });
 });
 
