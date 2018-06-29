7ddb8b3145fa2e9452334c994fb06b825ea00187
Eric Liu <ericliu404@gmail.com>
2018-06-29 14:41:28 -0400

    Fix: Dependency issues - Gulp to 4.0.0, Karma removed

--- gulpfile.js
+++ gulpfile.js
@@ -86,13 +86,12 @@ gulp.task('lint-src', lintSrc);
 gulp.task('lint-test', lintTest);
 gulp.task('lint-gulpfile', lintGulpfile);
 
-gulp.task('develop:build-icons', ['convert-svg-to-png', 'make-sprites']);
 gulp.task('develop:build-plugins', plugins);
 gulp.task('develop:build-diva', diva);
 gulp.task('develop:clean', cleanDist);
 gulp.task('develop:tmp-clean', cleanTemp);
-gulp.task('develop:lint', ['lint-src', 'lint-test', 'lint-gulpfile']);
+gulp.task('develop:lint', gulp.series('lint-src', 'lint-test', 'lint-gulpfile'));
 gulp.task('develop:server', server);
-gulp.task('develop', ['develop:lint', 'develop:clean', 'develop:build-plugins', 'develop:server']);
-gulp.task('develop:rodan', ['develop:lint', 'develop:clean', 'develop:build-plugins', 'develop:build-diva']);
-gulp.task('default', ['develop']);
+gulp.task('develop', gulp.series('develop:lint', 'develop:clean', 'develop:build-plugins', 'develop:server'));
+gulp.task('develop:rodan', gulp.series('develop:lint', 'develop:clean', 'develop:build-plugins', 'develop:build-diva'));
+gulp.task('default', gulp.series('develop'));
